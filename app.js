4b5ee39b520c34e7d55b681f311f9d2d962c129b
Tejaskumar Patel <pateltejas1812@gmail.com>
2019-05-30 13:18:58 -0400

    This is lab 1

---
+++ app.js
@@ -0,0 +1,22 @@
+//declaring variable that require exprees package 
+const express = require('express');
+
+//a variable that that initates express
+const app = express();
+
+//our home page of our app that shows contents detailing your routes
+app.get('/', (req, res) => {
+    res.send(`This show the all files of Lab.`);
+});
+
+//this is a about page that shows our group details 
+app.get('/about', (req, res) => {
+    res.send(`Our group member name is Tejaskumar Patel,Samarth Patel,Priyak Patel,Meghna Meghna. We All are from india.`);
+});
+
+//this contact page of our app that inform ways to contact us 
+app.get('/contact', (req, res) => {
+    res.send(`You can contact us by our student email or slack.`);
+});
+
+app.listen(880, () =>  console.log('Listining on port 880'));
